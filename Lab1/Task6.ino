6af59d81dbe1355593132cc2f65c80ee08454fbf
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2023-10-05 11:27:41 +0500

    fix

--- Lab1/Task6.ino
+++ Lab1/Task6.ino
@@ -1,15 +1,12 @@
 // Светодиод подключенный к пину D0 меняет яркость при повороте потенциометра подключенного к пину D1 с частотой 60Гц (ШИМ-сигнал).
-int ledPin = 0;
-int potPin = 1;
-int potValue = 0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(D0, OUTPUT);
+  analogWriteFreq(120);
 }
 
 void loop() {
-  potValue = analogRead(potPin); // Считываем значение потенциометра (0-1023)
-  int brightness = map(potValue, 0, 1023, 0, 255); // Преобразуем значение в диапазон яркости (0-255)
-  analogWrite(ledPin, brightness); // Устанавливаем яркость светодиода
-  delay(16); // Задержка для частоты 60 Гц (1 секунда / 60 Гц ≈ 16 миллисекунд)
+  int potValue = analogRead(A0);
+  int brightness = map(potValue, 0, 4095, 0, 255);
+  analogWrite(D0, brightness);
 }
